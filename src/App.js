73d4fc48cc52fd22d2c5a704551ff3e5a17222d0
anh <phamquockhuong2000@gmail.com>
2022-09-07 10:33:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({num1, num2}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-amber-800 text-2xl font-bold">
+          {num1}+{num2} = {num1+num2}
+      </h1>
     </div>
   );
 }
---
+++ src/components/header/headerTop/Navarbar.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Navarvar() {
+    return (
+        <>
+            <div>
+                div
+            </div>
+        </>
+    );
+}
+
+export default App;

81b4af5ba5d5df511aff3d60d6f9723d6ff9f584
anh <phamquockhuong2000@gmail.com>
2023-10-24 21:18:20 +0700

    refactor code

--- src/components/body/Cartphotoservice.js
+++ src/components/body/Cartphotoservice.js
@@ -6,8 +6,8 @@ function Carttphotoservice() {
                 <div className="w-6">
                     <img className='w-full h-auto' src={"/images/e_list.svg"} alt="" />
                 </div>
-                <span className="mx-2 text-[#314659]">身份证 chuc nang moi</span>
-                <span className="mx-2 text-[#314659]">身份证 thay doi</span>
+                <span className="mx-2 text-[#314659]">身份证</span>
+                <span className="mx-2 text-[#314659]">身份证</span>
             </div>
 
             <p className="my-2 text-[rgba(49,70,89,.75)]">识别身份证正反面姓名、身份证号、发证机关等相关信息</p>
